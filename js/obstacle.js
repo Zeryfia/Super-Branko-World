b7bf63a63d88f1779d2120a0c990db2cf3f2a4df
Zeryfia <72103922+Zeryfia@users.noreply.github.com>
2024-02-17 14:00:12 +0100

    Update obstacle.js

--- js/obstacle.js
+++ js/obstacle.js
@@ -50,7 +50,7 @@ function createObstacle() {
   const listOb = ['tag','div'];
   let random = Math.floor(Math.random()*listOb.length);
   let currentOb = listOb[random];
-  const pictures = [ {name: 'tag', src: `${process.env.PUBLIC_URL}../images/obstacle.png`}, {name: 'div', src: `${process.env.PUBLIC_URL}../images/div.png`}];
+  const pictures = [ {name: 'tag', src: `../images/obstacle.png`}, {name: 'div', src: `../images/div.png`}];
   // `${process.env.https://zeryfia.github.io/}../images/obstacle.png}'
   let temp = pictures.filter(item => item.name === `${currentOb}`);
   obstacle.src = temp[0].src;
